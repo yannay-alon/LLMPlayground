43d96f5d80489b4f9bb63fe649fa27f80398b12a
yannay-alon <yannayalon@gmail.com>
2025-03-15 10:03:36 +0200

    Adding tokenizer support for initial models

---
+++ model_utilities/tokenizer/__init__.py
@@ -0,0 +1,5 @@
+from model_utilities.tokenizer.tokenizer_loader import get_tokenizer
+
+__all__ = [
+    "get_tokenizer"
+]
---
+++ model_utilities/tokenizer/tokenizer_loader.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
+
+from utilities.model_family import ModelFamily
+
+__tokenizer_directory = Path(__file__).parent
+
+
+def get_tokenizer(model_name: str) -> PreTrainedTokenizerBase:
+    model_family = ModelFamily.infer_family(model_name)
+    return AutoTokenizer.from_pretrained(__tokenizer_directory / model_family)
