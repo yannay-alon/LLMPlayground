c0a3edd3079eb047f9576aad582c6753b8ca366b
yannay-alon <yannayalon@gmail.com>
2025-03-15 10:52:52 +0200

    Automatic connection details detection for model factory

--- models/model_factory.py
+++ models/model_factory.py
@@ -4,7 +4,7 @@ from models.api_model import APIModel
 from models.openai_model import OpenAIModel
 from models.cohere_model import CohereModel
 
-from model_utilities import ModelFamily
+from model_utilities import ModelFamily, ConnectionDetails
 
 
 class ModelFactory:
@@ -18,6 +18,11 @@ class ModelFactory:
             base_url: str | None = None,
             **kwargs
     ) -> APIModel:
+        if api_key is None:
+            api_key = ConnectionDetails.get_api_key(model_name)
+        if base_url is None:
+            base_url = ConnectionDetails.get_base_url(model_name)
+
         model_family = ModelFamily.infer_family(model_name)
 
         if model_family in [ModelFamily.COMMAND_A, ModelFamily.COMMAND_R]:
