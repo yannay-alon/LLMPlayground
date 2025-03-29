c378f584fdd597d84a8576ea563ad4a8708eda1d
yannay-alon <yannayalon@gmail.com>
2025-03-29 18:33:36 +0300

    Removing Cohere's model

--- models/model_factory.py
+++ models/model_factory.py
@@ -2,7 +2,6 @@ import warnings
 
 from models.api_model import APIModel
 from models.openai_model import OpenAIModel
-from models.cohere_model import CohereModel
 
 from model_utilities import ModelFamily, ConnectionDetails
 
@@ -24,9 +23,6 @@ class ModelFactory:
             base_url = ConnectionDetails.get_base_url(model_name)
 
         model_family = ModelFamily.infer_family(model_name)
-
-        if model_family in [ModelFamily.COMMAND_A, ModelFamily.COMMAND_R]:
-            return CohereModel(model_name, api_key, base_url, **kwargs)
 
         warnings.warn(
             f"Could not find a specific model class for {model_name}. Defaults to {cls.default_model_class.__name__}")
