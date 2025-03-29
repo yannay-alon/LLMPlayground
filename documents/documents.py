50ce9b4fe38bcfb476a494e12805c7ca39174eb3
yannay-alon <yannayalon@gmail.com>
2025-03-29 18:46:40 +0300

    Simpler document structure

--- documents/documents.py
+++ documents/documents.py
@@ -1,22 +1,11 @@
-from pydantic import BaseModel, Field, ConfigDict, model_serializer
+from pydantic import BaseModel, Field, ConfigDict
 
 
 class Document(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
-    identifier: str = Field(alias="id")
-    title: str
-    text: str
+    identifier: str | None = Field(default=None, alias="id")
+    content: str
 
-    @model_serializer
-    def serialize_model(self) -> dict[str, str | dict[str, str]]:
-        return {
-            "id": self.identifier,
-            "data": self._serialize_data()
-        }
-
-    def _serialize_data(self) -> dict[str, str]:
-        return {"title": self.title, "text": self.text}
-
-    def __str__(self):
-        return f"{self.title}\n{self.text}"
+    def __str__(self) -> str:
+        return self.content
