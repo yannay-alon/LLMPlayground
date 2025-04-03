1f09974f6d2634ab6bad849d101a211be181305a
yannay-alon <yannayalon@gmail.com>
2025-04-03 16:18:07 +0300

    Removing Cohere-specific model

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,10 @@
 from models.api_model import APIModel
 from models.openai_model import OpenAIModel
-from models.cohere_model import CohereModel
 from models.model_factory import ModelFactory
 
 __all__ = [
     "APIModel",
     "OpenAIModel",
-    "CohereModel",
 
     "ModelFactory"
 ]
