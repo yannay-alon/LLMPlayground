09f9fcf00c75d5fcb88b78a323fc005f8d69375e
yannay-alon <yannayalon@gmail.com>
2025-04-13 12:05:10 +0300

    Allowing for creating tool from class

--- components/tools/tools.py
+++ components/tools/tools.py
@@ -32,13 +32,33 @@ class Tool(BaseModel, Generic[ToolInput, ToolOutput]):
     def __init__(self, *args, **kwargs):
         # Check whether this is used as a decorator
         function = None
-        if len(args) == 0 and len(kwargs) == 1 and "function" in kwargs:
-            function = kwargs.pop("function")
-        elif len(args) == 1 and len(kwargs) == 0 and isinstance(args[0], (Tool, Callable)):
-            function = args[0]
+        functional_class = None
+        tool = None
+        if len(args) == 0 and len(kwargs) == 1:
+            if "function" in kwargs:
+                function = kwargs.pop("function")
+                assert inspect.isfunction(function), "the 'function' argument must be a function"
+            elif "class" in kwargs:
+                functional_class = kwargs.pop("class")
+                assert inspect.isclass(functional_class), "the 'class' argument must be a class"
+            elif "tool" in kwargs:
+                tool = kwargs.pop("tool")
+                assert isinstance(tool, Tool), "the 'tool' argument must be a Tool"
+        elif len(args) == 1 and len(kwargs) == 0:
+            argument = args[0]
+            if isinstance(argument, Tool):
+                tool = argument
+            elif inspect.isclass(argument):
+                functional_class = argument
+            elif inspect.isfunction(argument):
+                function = argument
 
         if function is not None:
             tool = self.from_function(function)
+        elif functional_class is not None:
+            tool = self.from_class(functional_class)
+
+        if tool is not None:
             super().__init__(
                 name=tool.name,
                 description=tool.description,
@@ -49,8 +69,8 @@ class Tool(BaseModel, Generic[ToolInput, ToolOutput]):
             super().__init__(**kwargs)
 
     @classmethod
-    def from_function(cls, function: Callable[ToolInput, ToolOutput] | "Tool") -> Self:
-        if isinstance(function, Tool):
+    def from_function(cls, function: Callable[ToolInput, ToolOutput] | Self) -> Self:
+        if isinstance(function, cls):
             return deepcopy(function)
 
         signature = inspect.signature(function)
@@ -74,6 +94,40 @@ class Tool(BaseModel, Generic[ToolInput, ToolOutput]):
             description=description,
             arguments=arguments,
             function=function,
+        )
+
+    @classmethod
+    def from_class(cls, functional_class: type | Self) -> Self:
+        if issubclass(functional_class, cls):
+            return deepcopy(functional_class)
+
+        class_signature = inspect.signature(functional_class)
+        class_description, _ = documentation_descriptions(functional_class, class_signature)
+
+        call_signature = inspect.signature(functional_class.__call__)
+        _, call_arguments_descriptions = documentation_descriptions(functional_class.__call__, call_signature)
+
+        arguments = []
+        for parameter in call_signature.parameters.values():
+            if parameter.name in {"self", "cls"}:
+                continue
+
+            argument_description = call_arguments_descriptions.get(parameter.name, "")
+            argument_annotation = parameter.annotation if parameter.annotation != inspect.Parameter.empty else Any
+            arguments.append(
+                Argument(
+                    name=parameter.name,
+                    description=argument_description,
+                    annotation=argument_annotation,
+                    required=(parameter.default == inspect.Parameter.empty)
+                )
+            )
+
+        return cls(
+            name=functional_class.__name__,
+            description=class_description,
+            arguments=arguments,
+            function=functional_class.__call__,
         )
 
     def __call__(self, *args: ToolInput.args, **kwargs: ToolInput.kwargs) -> ToolOutput:
