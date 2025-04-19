0826a23c715c1b0b43c3e81720711fa4f6a8d338
yannay-alon <yannayalon@gmail.com>
2025-04-19 19:40:21 +0300

    Better type-hinting for structured output

--- components/responses/choice.py
+++ components/responses/choice.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from enum import StrEnum
-from typing import TypeVar
+from typing import TypeVar, Generic
 from components.responses.tool_call import ToolCall
 
 ParsedType = TypeVar("ParsedType", bound=BaseModel)
@@ -13,7 +13,7 @@ class FinishReason(StrEnum):
     NONE = "none"
 
 
-class Choice(BaseModel):
+class Choice(BaseModel, Generic[ParsedType]):
     content: str
     finish_reason: FinishReason
     tool_calls: list[ToolCall] | None = None
