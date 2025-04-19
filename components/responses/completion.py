a702e42de04fa5d6760e2e1258bd757887bd360d
yannay-alon <yannayalon@gmail.com>
2025-04-19 19:40:21 +0300

    Better type-hinting for structured output

--- components/responses/completion.py
+++ components/responses/completion.py
@@ -1,8 +1,10 @@
+from typing import Generic
+
 from pydantic import BaseModel
-from components.responses.choice import Choice
+from components.responses.choice import Choice, ParsedType
 from components.responses.usage import Usage
 
 
-class Completion(BaseModel):
-    choices: list[Choice]
+class Completion(BaseModel, Generic[ParsedType]):
+    choices: list[Choice[ParsedType]]
     usage: Usage | None = None
