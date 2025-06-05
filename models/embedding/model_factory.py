2c21270b7c50f60672365b8b234665697b8d5330
yannay-alon <yannayalon@gmail.com>
2025-06-05 23:36:56 +0300

    Add ModelFactory class for dynamic embedding instantiation

---
+++ models/embedding/model_factory.py
@@ -0,0 +1,23 @@
+from models.embedding.api_model import APIModel
+from models.embedding.openai_model import OpenAIModel
+
+from models.utilities import ConnectionDetails
+
+
+class ModelFactory:
+    default_model_class = OpenAIModel
+
+    @classmethod
+    def get_model(
+            cls,
+            model_name: str,
+            api_key: str | None = None,
+            base_url: str | None = None,
+            **kwargs
+    ) -> APIModel:
+        if api_key is None:
+            api_key = ConnectionDetails.get_api_key(model_name)
+        if base_url is None:
+            base_url = ConnectionDetails.get_base_url(model_name)
+
+        return cls.default_model_class(model_name, api_key, base_url, **kwargs)
